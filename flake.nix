02205e893a55b6ea48175f1d48551c02bd733a93
Saurabh <jhanjeesaurabh@gmail.com>
2026-05-08 01:39:08 +0100

    feat: initial home-manager flake

---
+++ flake.nix
@@ -0,0 +1,18 @@
+{
+  description = "saurabhj home-manager configuration";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    homeConfigurations.saurabhj = home-manager.lib.homeManagerConfiguration {
+      pkgs = nixpkgs.legacyPackages.x86_64-linux;
+      modules = [ ./home.nix ];
+    };
+  };
+}
